f09cfc0738d649a5ffacb8e838b39905547f0ac0
Jenny Lin <misslinyang91@gmail.com>
2021-09-08 08:50:11 +1000

    created the game starter template

---
+++ app.js
@@ -0,0 +1,6 @@
+const canvasEl = document.querySelector("canvas");
+const canvasContext = canvasEl.getContext("2d");
+
+canvasEl.width = 1500;
+canvasEl.height = 720;
+// --------------------------------------------------------------------
