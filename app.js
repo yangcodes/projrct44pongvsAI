0c7c934de7b466b66148e5a3e75f66d19fd9f294
Jenny Lin <misslinyang91@gmail.com>
2021-09-08 09:33:26 +1000

    drew the elements

--- app.js
+++ app.js
@@ -4,3 +4,31 @@ const canvasContext = canvasEl.getContext("2d");
 canvasEl.width = 1500;
 canvasEl.height = 720;
 // --------------------------------------------------------------------
+
+//drawing the canvas
+function drawRect(xP, yP, width, height, color) {
+  (canvasContext.fillStyle = color),
+    canvasContext.fillRect(xP, yP, width, height);
+}
+
+//drawing the ball
+function drawCircle(xP, yP, radius, color) {
+  canvasContext.fillStyle = color;
+  canvasContext.beginPath();
+  canvasContext.arc(xP, yP, radius, 0, Math.PI * 2);
+  canvasContext.fill();
+}
+
+//drawing the text
+function drawText(content, xP, yP, color) {
+  canvasContext.fillStyle = color;
+  canvasContext.font = "35px sans-serif";
+  canvasContext.fillText(content, xP, yP);
+}
+
+//drawing the net
+function drawNet() {
+  for (let i = 0; i < canvasEl.height; i += 15) {
+    drawRect(net.xP, net.yP + i, net.width, net.height, net.color);
+  }
+}
