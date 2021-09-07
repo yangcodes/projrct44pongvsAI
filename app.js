a5c7d614242443c666ff38d05f1489e208f1b0ed
Jenny Lin <misslinyang91@gmail.com>
2021-09-08 09:46:35 +1000

    created the properties for the game elements

--- app.js
+++ app.js
@@ -4,6 +4,55 @@ const canvasContext = canvasEl.getContext("2d");
 canvasEl.width = 1500;
 canvasEl.height = 720;
 // --------------------------------------------------------------------
+let RIScore = new Audio();
+let AIScore = new Audio();
+let hit = new Audio();
+let wall = new Audio();
+
+hit.src = "sounds/hit.mp3";
+wall.src = "sounds/wall.mp3";
+AIScore = "sounds/AIScore.mp3";
+RIScore = "sounds/RIScore.mp3";
+
+//the RI player paddle
+const playerPaddleRI = {
+  xP: 0,
+  yP: canvasEl.height / 2 - 100 / 2,
+  height: 100,
+  width: 10,
+  color: "#d2e603",
+  score: 0,
+};
+
+//the AI player paddle
+const playerPaddleAI = {
+  xP: canvasEl.width - 10,
+  yP: canvasEl.height / 2 - 100 / 2,
+  height: 100,
+  width: 10,
+  color: "orange",
+  score: 0,
+};
+
+//creating the ball
+const ball = {
+  xP: canvasEl.width / 2,
+  yP: canvasEl.height / 2,
+  radius: 10,
+  speed: 7,
+  xV: 5,
+  yV: 5,
+  color: "white",
+};
+
+//creating the net
+const net = {
+  xP: canvasEl.width / 2 - 1,
+  yP: 0,
+  width: 2,
+  height: 10,
+  color: "white",
+};
 
 //drawing the canvas
 function drawRect(xP, yP, width, height, color) {
